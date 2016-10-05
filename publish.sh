2823bcd4877b4846e9789cb408da981b423888f7
bigzhu <vermiliondun@gmail.com>
2016-10-05 10:01:39 +0800

    push npm then push github

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-# sh ./change_name.sh
-# npm run build
+sh ./change_name.sh
+npm run build
+npm version patch
+npm publish ./
+
 commit='git commit -am "'$1'"'
 echo $commit
 eval $commit
-# npm version patch
-# npm publish ./
-# git push
+git push
